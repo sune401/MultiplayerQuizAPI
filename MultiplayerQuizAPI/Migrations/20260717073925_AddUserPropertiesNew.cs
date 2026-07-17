4b24aa952ac3b66552e052cf77a93982eadae528
sune4 <sune4@DESKTOP-9BFUDE9>
2026-07-17 09:40:17 +0200

    gg

---
+++ MultiplayerQuizAPI/Migrations/20260717073925_AddUserPropertiesNew.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MultiplayerQuizAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserPropertiesNew : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "phoneNumber",
+                table: "Users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "phoneNumber",
+                table: "Users");
+        }
+    }
+}
